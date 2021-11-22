1b4614d4665bad0cba16e9675d5cc89c9b4c9f55
Asiabill <tech@asiabill.com>
2021-11-22 18:59:09 +0800

    更新2.0SDK包

---
+++ AsiaBillPaySDKDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AsiaBillPaySDKDemo
+//
+//  Created by 邓侃 on 2021/7/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
